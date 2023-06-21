2b46f214c4194f027df9b89bb78a3dd5a9ca7f25
Zurab-Barnaveli <zuka.barnaveli11@gmail.com>
2023-06-21 20:36:04 +0400

    .

--- ecom3/src/utils/Ajax.ts
+++ ecom3/src/utils/Ajax.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const Ajax = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API,
   headers: { "Content-Type": "application/json" },
 });
