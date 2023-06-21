a77995ece6ba4d391c29f3cb87bc41b89d03392a
Zurab-Barnaveli <zuka.barnaveli11@gmail.com>
2023-06-21 18:55:59 +0400

    com

--- ecom3/src/utils/Ajax.ts
+++ ecom3/src/utils/Ajax.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const Ajax = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: "http://localhost:8080",
   headers: { "Content-Type": "application/json" },
 });
